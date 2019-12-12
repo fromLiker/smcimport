d1d5b1c669f4a65e914c88514ddbfbd49855a064
xuexp <xuexp@cn.ibm.com>
2019-12-12 12:31:20 +0800

    first commit

---
+++ src/test/java/com/smc/ImportApplicationTests.java
@@ -0,0 +1,30 @@
+package com.smc;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportApplicationTests {
+
+//	@Autowired
+//	private UsersService usersService;
+//
+//	@Test
+//	public void userLogin(){
+//		try {
+//			UsersDTO usersDTO = new UsersDTO();
+//			usersDTO.setEmail("wangrt@cn.ibm.com");
+//			usersDTO.setPassword("888888");
+//			CommonResult res = usersService.findUser(usersDTO);
+//	        Userinfolist users = new Userinfolist();
+//	        users = (Userinfolist) res.getData();
+//		  	System.out.println("1*****1******" + users.getEmail());
+//		    System.out.println("2*****2******" + users.getPassword());
+//	    	System.out.println("***********login succeed");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			System.out.println("***********login failed");
+//		}
+//	}
+
+}
